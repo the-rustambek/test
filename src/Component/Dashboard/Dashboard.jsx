51c082b4448207a50a30fc98fe0b51848de04512
rustambek-Mamadaliyev0301 <rustambek0301@gmail.com>
2023-01-22 00:57:43 +0500

    Add sidebar

---
+++ src/Component/Dashboard/Dashboard.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import "./Dashboard.css"
+const Dashboard = () => {
+    return (
+        <div className="d-flex flex-column flex-shrink-0  " style={{ width: "280px", background: "#51438F", height: "100vh" }}>
+            <ul className="nav nav-pills flex-column mb-auto">
+                <li className="nav-item">
+                    <Link to="#" className="navbar-link  text-white " >
+                        Profile
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="#" className="navbar-link  active" aria-current="page">
+                        Users
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/login" className="navbar-link  text-white">
+                        Logout
+                    </Link>
+                </li>
+            </ul>
+
+        </div>
+    )
+}
+
+export default Dashboard;
--- src/Component/HomePage/HomePage.jsx
+++ src/Component/HomePage/HomePage.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import Dashboard from '../Dashboard/Dashboard';
+import Navbar from '../Navbar/Navbar';
 
 function Homepage() {
   return (
-    <div>Homepage</div>
+    <div>
+      <Navbar />
+      <div className='d-flex '>
+        <Dashboard />
+        <div>main page</div>
+      </div>
+    </div>
   )
 }
 
